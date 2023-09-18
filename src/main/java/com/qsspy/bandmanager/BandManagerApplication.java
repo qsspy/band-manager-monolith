26180ddc4adbb5072afc33b7470fee06f20c9819
qsspy <danelfilipek@gmail.com>
2023-09-18 23:45:18 +0200

    init commit

---
+++ src/main/java/com/qsspy/bandmanager/BandManagerApplication.java
@@ -0,0 +1,13 @@
+package com.qsspy.bandmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsspy/bandmanager/BandManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsspy.bandmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
