c83118728ec4dbf1b948a8e70b9652b7271cf182
qsspy <danelfilipek@gmail.com>
2023-10-08 17:54:13 +0200

    Added missing event

---
+++ commons/src/main/java/com/qsspy/commons/architecture/port/output/publisher/MeasurementStartedNotificationEvent.java
@@ -0,0 +1,36 @@
+package com.qsspy.commons.architecture.port.output.publisher;
+
+import java.util.UUID;
+
+public record MeasurementStartedNotificationEvent(
+        UUID eventId,
+        long occurredOn,
+
+        MeasurementType measurementType,
+        int recordNumber,
+        int description
+) implements NotificationEvent {
+
+    private static final int EVENT_VERSION = 1;
+    private static final String EVENT_TYPE = "measurement.started";
+
+    @Override
+    public String eventType() {
+        return EVENT_TYPE;
+    }
+
+    @Override
+    public int eventVersion() {
+        return EVENT_VERSION;
+    }
+
+    @Override
+    public String destinationBindingName() {
+        return "measurements";
+    }
+
+    @Override
+    public Object partitionKey() {
+        return eventId;
+    }
+}
